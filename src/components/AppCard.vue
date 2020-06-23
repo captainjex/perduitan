ff7064510402a2f808a0d064570c3cba42ada1f8
gaspadat <mailpakgasing@gmail.com>
2020-06-23 15:34:53 +0700

    dummy page app

---
+++ src/components/AppCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="rounded-lg px-4 py-8" :class="selectedColor">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+const COLORS : { [key: string]: any } = {
+  green: 'text-white bg-green-400 hover:bg-green-500',
+  red: 'text-white bg-red-400 hover:bg-red-500'
+}
+
+export default Vue.extend({
+  props: {
+    color: {
+      type: String,
+      default: () => COLORS.green
+    }
+  },
+  computed: {
+    selectedColor (): string {
+      return COLORS[this.color]
+    }
+  }
+})
+</script>
--- src/pages/app/index.vue
+++ src/pages/app/index.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="text-4xl text-teal-400">
-    index app
+  <div class="bg-gray-100 min-h-screen flex flex-col py-8">
+    <h1 class="font-bold text-gray-900 text-2xl px-6">
+      Perduitan
+    </h1>
+    <div class="flex items-center text-base overflow-x-scroll">
+      <a v-for="i in listOfMonths" :key="i" class="mr-5 flex-shrink-0 hover:text-gray-900">{{ i }}</a>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  computed: {
+    listOfMonths (): string[] {
+      return [
+        'Bulan ini',
+        'Mei',
+        'April',
+        'Maret',
+        'Februari',
+        'Januari',
+        'Desember 2019',
+        'November 2019',
+        'Oktober 2019',
+        'September 2019'
+      ]
+    }
+  }
+})
+</script>
